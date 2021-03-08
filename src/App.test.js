3cae0ebd5a838066a360e6a9d022e03a8ed5f075
ambergfisher <50674241+ambergfisher@users.noreply.github.com>
2021-03-08 16:33:28 -0500

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,8 @@
+import React from 'react'
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders SASE name', () => {
   render(<App />);
   const sase_name = screen.getByText(/UF SASE/i);
   expect(sase_name).toBeInTheDocument();
